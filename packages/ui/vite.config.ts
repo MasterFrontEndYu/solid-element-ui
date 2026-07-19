19d2500e9a052d07199bff717acabce4f0edb90a
sanguogege <sanguogege@outlook.com>
2026-07-19 17:07:41 +0800

    add

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,33 +1,33 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vite-plus";
 import solidPlugin from "vite-plugin-solid";
 import { resolve } from "path";
 import dts from "vite-plugin-dts";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
-    plugins: [
-        solidPlugin(),
-        dts({
-            include: ["src"],
-            insertTypesEntry: true,
-            entryRoot: "src",
-        }),
-        tailwindcss(),
-    ],
-    build: {
-        lib: {
-            entry: resolve(__dirname, "src/index.tsx"),
-            formats: ["es"],
-            fileName: "index",
-        },
-        rollupOptions: {
-            external: [
-                "solid-js",
-                "solid-js/web",
-                "solid-js/store",
-                "@kobalte/core",
-                "tailwind-variants",
-                "tailwind-merge",
-            ],
-        },
+  plugins: [
+    solidPlugin(),
+    dts({
+      include: ["src"],
+      insertTypesEntry: true,
+      entryRoot: "src",
+    }),
+    tailwindcss(),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.tsx"),
+      formats: ["es"],
+      fileName: "index",
     },
+    rollupOptions: {
+      external: [
+        "solid-js",
+        "solid-js/web",
+        "solid-js/store",
+        "@kobalte/core",
+        "tailwind-variants",
+        "tailwind-merge",
+      ],
+    },
+  },
 });

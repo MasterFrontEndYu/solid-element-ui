fc9d054f6f98d3968e2917fe323754cb49e7e75c
sanguogege in wsl <sanguogege@outlook.com>
2026-08-16 12:59:40 +0800

    啊大大

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,15 +1,18 @@
 import { defineConfig } from "vite-plus";
 import solid from "@solidjs/vite-plugin";
 import { resolve } from "path";
+import dts from "vite-plugin-dts";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
-  plugins: [solid(), tailwindcss()],
-  pack: {
-    dts: {
-      tsgo: true,
-    },
-    exports: true,
-  },
+  plugins: [
+    solid(),
+    dts({
+      include: ["src"],
+      insertTypesEntry: true,
+      entryRoot: "src",
+    }),
+    tailwindcss(),
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.tsx"),

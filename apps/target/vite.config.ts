cd2484ec0ce65ddb52a2b5019be198c0785f2b4f
sanguogege <sanguogege@outlook.com>
2026-08-19 21:36:28 +0800

    fix zhe version

--- apps/target/vite.config.ts
+++ apps/target/vite.config.ts
@@ -1,19 +1,11 @@
 import { fileRoutes } from "filesystem-routing/vite";
-import { defineConfig } from "vitest/config";
+import { defineConfig } from "vite-plus";
 import solid from "@solidjs/vite-plugin";
 
 export default defineConfig({
   plugins: [solid({ start: true, extensions: [".jsx", ".tsx"] }), fileRoutes({ types: true })],
   server: {
     port: 3000,
-  },
-  test: {
-    environment: "jsdom",
-    globals: false,
-    setupFiles: ["./vitest-setup.ts"],
-    // if you have few tests, try commenting this
-    // out to improve performance:
-    isolate: false,
   },
   build: {
     target: "esnext",

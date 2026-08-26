36a08924738a263f8987bb58d4d9998c2d057170
sanguogege <sanguogege@outlook.com>
2026-08-27 00:16:05 +0800

    111

--- packages/ui/core/accordion/accordion.tsx
+++ packages/ui/core/accordion/accordion.tsx
@@ -3,9 +3,9 @@ import { For, omit } from "solid-js";
 import type { JSX } from "@solidjs/web";
 import { ChevronDown } from "../icons";
 
-import { defaultClass} from "./setting"
+import { defaultClass } from "./setting";
 
-
+// TODO  别忘记如何处理 后端返回的 带标签的数据？强制用户自己处理，组件只用处理jsx就可以了。
 
 export const Accordion = (props: any) => {
   const others = omit(props, "items", "class");
@@ -14,7 +14,11 @@ export const Accordion = (props: any) => {
     <KAccordion class={defaultClass.root} {...others}>
       <For each={props.items}>
         {(itemData) => (
-          <KAccordion.Item value={itemData.value} disabled={itemData.disabled} class={defaultClass.item}>
+          <KAccordion.Item
+            value={itemData.value}
+            disabled={itemData.disabled}
+            class={defaultClass.item}
+          >
             <KAccordion.Header class={defaultClass.header}>
               <KAccordion.Trigger class={defaultClass.trigger}>
                 {itemData.title}
